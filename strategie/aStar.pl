830a52abeefd41d1f9f66a28c7fa796a5e36804e
Dario Franzese <dariofranzese@hotmail.com>
2024-04-09 11:43:47 +0200

    minor fixes checkvisitati
    aggiunti dei don'tcares

--- strategie/aStar.pl
+++ strategie/aStar.pl
@@ -11,23 +11,24 @@ algoritmoAStar:-
 aStar([Corrente, Path, _], _, _, Path):-
     finale(Corrente), !.
 
-aStar([Corrente, Path, Valutazione], Frontiera, Visitati, Risultato):-
+aStar([Corrente, Path, Valutazione], Frontiera, Visitati, _):-
     findall(Azione, applicabile(Azione, Corrente), Azioni),
     checkVisitati(Visitati, Corrente, Azioni, NuoviVisitati),
-    generaStati(Corrente, Path, Azioni, Frontiera, NuoviVisitati,
-         [TestaDellaNuovaFrontiera | CodaDellaNuovaFrontiera]),
-    aStar(TestaDellaNuovaFrontiera, CodaDellaNuovaFrontiera, _, _).
+    generaStati(Corrente, Path, Valutazione, Azioni, Frontiera, NuoviVisitati,
+         [TestaDellaNuovaFrontiera | CodaDellaNuovaFrontiera]), %Questo e' l' output
+    aStar(TestaDellaNuovaFrontiera, CodaDellaNuovaFrontiera, NuoviVisitati, _).
 
 
-checkVisitati(NuoviVisitati, _, [], NuoviVisitati).%:- write(NuoviVisitati).
-checkVisitati(Visitati, Corrente, [Azione | CodaAzioni], NuoviVisitati):-
+checkVisitati(NuoviVisitati, _, [], NuoviVisitati).
+checkVisitati(Visitati, Corrente, [Azione | CodaAzioni], _):-
     trasforma(Azione, Corrente, NuovoStato),
     \+member(NuovoStato, Visitati),
-    checkVisitati([NuovoStato | Visitati], Corrente, CodaAzioni, NuoviVisitati).
+    checkVisitati([NuovoStato | Visitati], Corrente, CodaAzioni, _).
 
 checkVisitati(Visitati, Corrente, [ _ | CodaAzioni], _):-
     checkVisitati(Visitati, Corrente, CodaAzioni, _), !.
 
 
-%generaStati(Corrente, Path, Azioni, Frontiera, Visitati, ):-
+generaStati(Corrente, Path, Valutazione, Azioni, Frontiera, Visitati, _):-
+    
 
