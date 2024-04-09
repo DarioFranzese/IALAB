21015e403b27212cfd53e2c0b7d0ce832cfdb8f1
Dario Franzese <dariofranzese@hotmail.com>
2024-04-09 11:26:17 +0200

    fix checkVisitati
    La cat era stata inserita nel caso sbagliato, adesso funziona bene.

--- strategie/aStar.pl
+++ strategie/aStar.pl
@@ -18,14 +18,15 @@ aStar([Corrente, Path, Valutazione], Frontiera, Visitati, Risultato):-
          [TestaDellaNuovaFrontiera | CodaDellaNuovaFrontiera]),
     aStar(TestaDellaNuovaFrontiera, CodaDellaNuovaFrontiera, _, _).
 
-checkVisitati(NuoviVisitati, _, [], NuoviVisitati):- write(NuoviVisitati).
+
+checkVisitati(NuoviVisitati, _, [], NuoviVisitati).%:- write(NuoviVisitati).
 checkVisitati(Visitati, Corrente, [Azione | CodaAzioni], NuoviVisitati):-
     trasforma(Azione, Corrente, NuovoStato),
-    \+member(NuovoStato, Visitati),!,
+    \+member(NuovoStato, Visitati),
     checkVisitati([NuovoStato | Visitati], Corrente, CodaAzioni, NuoviVisitati).
 
 checkVisitati(Visitati, Corrente, [ _ | CodaAzioni], _):-
-    checkVisitati(Visitati, Corrente, CodaAzioni, _).
+    checkVisitati(Visitati, Corrente, CodaAzioni, _), !.
 
 
 %generaStati(Corrente, Path, Azioni, Frontiera, Visitati, ):-
