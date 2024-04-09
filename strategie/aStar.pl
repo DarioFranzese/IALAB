e46aa0bbbbd34c26a617f1e371b8750dca93e59c
Dario Franzese <dariofranzese@hotmail.com>
2024-04-09 12:40:45 +0200

    Ristrutturazione check visitati e genera stati
    Check visitati adesso si occupa di controllare se i nuovi stati sono gia' stati visitati e di restituire una copia dei nuovi validi (partendo dalle azioni), genera stati si dovrebbe occupare solo di inserire ordinato in frontiera, la chiamata successiva in aStar dovrebbe essere a quel punto solo quella ricorsiva (con le varie varibaili aggiornate)

--- strategie/aStar.pl
+++ strategie/aStar.pl
@@ -13,22 +13,23 @@ aStar([Corrente, Path, _], _, _, Path):-
 
 aStar([Corrente, Path, Valutazione], Frontiera, Visitati, _):-
     findall(Azione, applicabile(Azione, Corrente), Azioni),
-    checkVisitati(Visitati, Corrente, Azioni, NuoviVisitati),
-    generaStati(Corrente, Path, Valutazione, Azioni, Frontiera, NuoviVisitati,
+    checkVisitati(Visitati, Corrente, Azioni, [], NuoviStati), %NuoviStati sono i nuovi stati generati, visitati ora e' di input output e viene aggiornata
+    generaStati(Corrente, Path, Valutazione, NuoviStati, Frontiera,
          [TestaDellaNuovaFrontiera | CodaDellaNuovaFrontiera]), %Questo e' l' output
-    aStar(TestaDellaNuovaFrontiera, CodaDellaNuovaFrontiera, NuoviVisitati, _).
+    aStar(TestaDellaNuovaFrontiera, CodaDellaNuovaFrontiera, Visitati, _).
 
 
-checkVisitati(NuoviVisitati, _, [], NuoviVisitati).
-checkVisitati(Visitati, Corrente, [Azione | CodaAzioni], _):-
+checkVisitati(_, _, [], NuoviStati, NuoviStati).
+
+checkVisitati(Visitati, Corrente, [Azione | CodaAzioni], NuoviStati, _):-
     trasforma(Azione, Corrente, NuovoStato),
     \+member(NuovoStato, Visitati),
-    checkVisitati([NuovoStato | Visitati], Corrente, CodaAzioni, _).
+    checkVisitati([NuovoStato | Visitati], Corrente, CodaAzioni, [NuovoStato | NuoviStati], _).
 
-checkVisitati(Visitati, Corrente, [ _ | CodaAzioni], _):-
-    checkVisitati(Visitati, Corrente, CodaAzioni, _), !.
+checkVisitati(Visitati, Corrente, [ _ | CodaAzioni], NuoviStati, _):-
+    checkVisitati(Visitati, Corrente, CodaAzioni, NuoviStati, _), !.
 
-
-generaStati(Corrente, Path, Valutazione, Azioni, Frontiera, Visitati, _):-
-    
+%genera stati in realta' deve solo aggiungere i nuovi stati alla frontiera
+generaStati(Corrente, Path, Valutazione, NuoviStati, Frontiera,
+         [TestaDellaNuovaFrontiera | CodaDellaNuovaFrontiera]).
 
