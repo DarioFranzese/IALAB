69c7525ac2bfccb5e2faf549332b2e557e543f24
Dario Franzese <dariofranzese@hotmail.com>
2024-06-01 18:24:00 +0200

    fix applicabile
    per testare applicabile, modificare la lista presente in "mostriciattolo1.lp" a piacimento e runnare il comando:
    movibili(M), iniziale(S), applicabile(*, S, M) sostituendo all' asterisco uno tra {nord, sud, est, ovest}. Se stampa le variabili S e M significa che l' azione e' applicabile (il predicato ha dato true), altrimenti restituira' false. Per fare debuging di quale dei due "checkAvversario" e' fallita mettete banalmente una stampa tra le due chiamate.

--- PROLOG/mostriciattolo/applicabile.pl
+++ PROLOG/mostriciattolo/applicabile.pl
@@ -12,44 +12,40 @@ checkAvversario(sud, R, _, _):- num_colonne(R).
 checkAvversario(_, R, C, _):- occupata(pos(R,C)).
 checkAvversario(_, R, C, Movibili):- member(gemma(pos(R,C)), Movibili).
 checkAvversario(_, R, C, _):- finale(pos(R, C)).
-checkAvversario(_, R, C, [martello(pos(_, _)) | _ ]):- ghiaccio(pos(R, C)). %E' UN CASO BLOCCANTE SOLO SE NON HO IL MARTELLO.
+checkAvversario(_, R, C, [martello(pos(_, _)) | Movibili ]):- member(ghiaccio(pos(R,C)), Movibili). %E' UN CASO BLOCCANTE SOLO SE NON HO IL MARTELLO.
 
 %CASO SPECIALE QUANDO CONTROLLO CHE L' AVVERSARIO NON MI FINISCA ADDOSSO: IL MARTELLO FUNGE DA OSTACOLO ALL' AVVERSARIO
 checkAvversario(_, R, C, [martello(pos(-1,-1)) | Movibili]):- member(martello(pos(R,C)), Movibili). 
 
-
-%MI FERMO E FALLISCO PERCHE' HO TROVATO L' AVVERSARIO PRIMA DI QUALCHE OSTACOLO
-checkAvversario(_, R, C, Movibili):- member(avversario(pos(R, C)), Movibili), fail, !. %da controlalre se il cut va prima
-
+%CASO IN CUI "PRENDEREI" IL MARTELLO, RICHIAMA SE STESSO SIMULANDO LA PRESA DEL MARTELLO (toglierlo dai movibili)
+%questo dopo andra' sicuramente avanti ma avrei dovuto fare 4 predicati diversi a seconda dell' azione, preferisco fargli perdere tempo anziche' mettermi a scriverle io
+checkAvversario(Azione, R, C, [martello(pos(R, C)) | CodaMovibili]):- !, checkAvversario(Azione, R, C, CodaMovibili).
 
 %CHIAMATE RICORSIVE PER PROSEGUIRE LA RICERCA
-checkAvversario(nord, R, C, Movibili):- NR is R-1, checkAvversario(nord, NR, C, Movibili).
-checkAvversario(sud, R, C, Movibili):- NR is R+1, checkAvversario(sud, NR, C, Movibili).
-checkAvversario(est, R, C, Movibili):- NC is C+1, checkAvversario(est, R, NC, Movibili).
-checkAvversario(ovest, R, C, Movibili):- NC is C-1, checkAvversario(ovest, R, NC, Movibili).
+checkAvversario(nord, R, C, Movibili):- NR is R-1, \+member(avversario(pos(NR, C)), Movibili), !, checkAvversario(nord, NR, C, Movibili).
+checkAvversario(sud, R, C, Movibili):-  NR is R+1, \+member(avversario(pos(NR, C)), Movibili), !, checkAvversario(sud, NR, C, Movibili).
+checkAvversario(est, R, C, Movibili):-  NC is C+1, \+member(avversario(pos(R, NC)), Movibili), !, checkAvversario(est, R, NC, Movibili).
+checkAvversario(ovest, R, C, Movibili):- NC is C-1, \+member(avversario(pos(R, NC)), Movibili), !, checkAvversario(ovest, R, NC, Movibili).
 
-%CASO IN CUI "PRENDEREI" IL MARTELLO, RICHIAMA SE STESSO SIMULANDO LA PRESA DEL MARTELLO (toglierlo dai movibili)
-checkAvversario(Azione, R, C, [martello(pos(R, C)) | CodaMovibili]):- checkAvversario(Azione, R, C, CodaMovibili).
+
 
 
 
 %Daro' per scontato che il martello e' sempre il primo elemento della lista dei movibili, se questo non dovesse accadere vanno cambiati i due predicati che anziche' usare Movibili usano la lista esplicita
 %con il martello in testa
 applicabile(nord, pos(R, C), Movibili):-
-    checkAvversario(nord, R, C, Movibili), %prima controllo che non ci sia sulla traiettoria
+    checkAvversario(nord, R, C, Movibili),!, %prima controllo che non ci sia sulla traiettoria
     checkAvversario(sud, R, C, [martello(pos(-1,-1)) | Movibili]),!. %poi controllo che "non mi venga addosso"
                                                                    %l' inserire martello(pos(-1, -1)) in testa e' un trucchetto per distinguere le due chiamate qui. In particolare questo' fara' attivare
                                                                    %il predicato per il quale se trovo un martello mi fermo (e di conseguenza non si attivera' mai quello che simula la presa del martello)
-
-
 applicabile(sud, pos(R, C), Movibili):-
-    checkAvversario(sud, R, C, Movibili),
+    checkAvversario(sud, R, C, Movibili),!,
     checkAvversario(nord, R, C, [martello(pos(-1,-1)) | Movibili]),!.
 
 applicabile(est, pos(R, C), Movibili):-
-    checkAvversario(est, R, C, Movibili),
+    checkAvversario(est, R, C, Movibili),!,
     checkAvversario(ovest, R, C, [martello(pos(-1,-1)) | Movibili]),!.
 
 applicabile(ovest, pos(R, C), Movibili):-
-    checkAvversario(ovest, R, C, Movibili),
+    checkAvversario(ovest, R, C, Movibili),!,
     checkAvversario(est, R, C, [martello(pos(-1,-1)) | Movibili]),!.
--- PROLOG/mostriciattolo/labirinti/mostriciattolo1.pl
+++ PROLOG/mostriciattolo/labirinti/mostriciattolo1.pl
@@ -30,9 +30,8 @@ ghiaccio(pos(7,7)).
 
 martello(pos(8,2)).
 
-avversario(pos(8,4)).
+avversario(pos(1,1)).
 
-%MODIFICA LA POSIZIONE DELL' AVVERSARIO QUI
-movibili([martello(pos(8,2)), gemma(pos(1,7)), gemma(pos(5,4)),gemma(pos(8,8)), ghiaccio(pos(2,6)), ghiaccio(pos(2,7)), ghiaccio(pos(7,7)), avversario(pos(3,4))]). %IL MARTELLO E' IN TESTA!+
+movibili([martello(pos(8,2)), gemma(pos(1,7)), gemma(pos(5,4)),gemma(pos(8,8)), ghiaccio(pos(2,6)), ghiaccio(pos(2,7)), ghiaccio(pos(7,7)), avversario(pos(1,1))]).
 
 
